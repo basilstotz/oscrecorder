1bc4c4bacc8fbf1da65d880591dd43b32b722249
Basil Stotz <stotz.basil@opinsys.fi>
2023-05-19 17:22:42 +0200

    add info option

--- oscrecorder/opt/oscrecorder/oscplay2.js
+++ oscrecorder/opt/oscrecorder/oscplay2.js
@@ -10,7 +10,6 @@ usage:   oscplay [options] filename
        
 options: --help,-h                   : show this message
          --speed,-s <speed>          : set replay speed to <speed> (default: 1.0)
-         --timeOffset,t <timeOffset> : set timeOffset to <timeOffset> ms. default is measured file readtime
          --loop,-l                   : loop forever 
 
          Read osc-bundles from <filename> and dump them, using the relative time 
@@ -21,26 +20,27 @@ let file='';
 let speed=1.0;
 let timeOffset;
 let loop=false;
+let info=false;
 
 const Args = process.argv.slice(2);
 for(let i=0;i<Args.length;i++){
     switch(Args[i]){
-     case '--timeoffset':
-     case '-t':	
-	timeOffset=Math.round(Args[++i]);
-	break;
-     case '--speed':
-     case '-s':
-	 speed=1.0/Args[++i];
-	 break;
      case '-h':
      case '--help':
 	 help();
 	 process.exit();
 	 break;
+     case '--speed':
+     case '-s':
+	 speed=1.0/Args[++i];
+	 break;
      case '-l':
      case '--loop':
 	loop=true;
+	 break;
+     case '-i':
+     case '--indo':
+	info=true;
 	 break;
      default:
  	 if(!file){
@@ -71,6 +71,16 @@ let line;
 {"offset":0,"timetag":{"value":{"seconds":3892649641,"fractions":38654976},"offset":0},"bundleElements":[{"offset":36,"address":"/uhu","types":",i","args":[43]}]}
 */
 
+function laenge(duration){
+    let r;
+    let h=Math.floor(duration/3600000);
+    r=duration-h*3600000;
+    let m=Math.floor(r/60000);
+    r=r-m*60000;
+    let s=Math.floor(r/1000);
+    r=r-s*1000;
+    return h+'h '+m+'m '+s+'.'+r+'s';
+}
 
 function  out(item){
     let timetag;
@@ -107,6 +117,17 @@ while(line = liner.next()){
 }
 duration=maxTime-minTime;
 
+if(info){
+    console.log('---------------------------------------------------------');
+    console.log('|     File: '+file);
+    console.log('---------------------------------------------------------');
+    console.log('|    Start: '+new Date(minTime).toString());
+    console.log('|     Ende: '+new Date(maxTime).toString());
+    console.log('|    Länge: '+laenge(duration));
+    console.log('| Messages: '+bundles.length);
+    console.log('---------------------------------------------------------\n');
+    process.exit();
+}
 
 if(!timeOffset)timeOffset=new Date().getTime()-start;
 
